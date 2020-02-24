23fec455a090c896f384c4d4cdf66d1603d7f557
snehabanga2002 <60182286+snehabanga2002@users.noreply.github.com>
2020-02-24 17:33:46 +0530

    Create Solution2.py

---
+++ Solution2.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+a = list(map(str,input().split()))
+m = int(max("".join(i)) for i in permuations(str(i) for i in e)),key = int())
+print(str(m))
+
+
+
+INPUT:
+    [10,76,7,415]
+OUTPUT:
+    77641510
